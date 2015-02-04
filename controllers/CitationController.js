3d129b357b8c9fe272fbd3f687a92d7272a9ac3b
benitol87 <benoit.leroy@etu.unilim.fr>
2015-02-04 11:01:24 +0100

    Liste des citations

--- controllers/CitationController.js
+++ controllers/CitationController.js
@@ -1,28 +1,38 @@
-
-   
+var model = require('../models/citation.js');
+	 
 // ////////////////////////////////////////////// L I S T E R     C I T A T I O N 
-   
+	 
 module.exports.ListerCitation = 	function(request, response){
-   response.title = 'Liste des citations';
-  
-      response.render('listerCitation', response);
-  } ;   
+	response.title = 'Liste des citations';
+	
+	model.getListeCitation( function (err, result) {
+		if (err) {
+				// gestion de l'erreur
+				console.log(err);
+				return;
+		}
+		response.listeCitation = result; 
+		response.nbCitation = result.length;
+		response.render('listerCitation', response);
+	}); 
+			
+	} ;   
 
 // ////////////////////////////////////////////// A J O U T E R     C I T A T I O N 
-   
+	 
 module.exports.AjouterCitation = 	function(request, response){
-	   response.title = 'Ajouter des citations';
-   response.render('ajouterCitation', response);
-     
-  } ;   
+		 response.title = 'Ajouter des citations';
+	 response.render('ajouterCitation', response);
+		 
+	} ;   
 
 
 // ////////////////////////////////////////////// R E C H E R C H E R     C I T A T I O N 
-   
+	 
 module.exports.RechercherCitation = function(request, response){
-   response.title = 'Rechercher des citations';
-   response.render('rechercherCitation', response);
+	 response.title = 'Rechercher des citations';
+	 response.render('rechercherCitation', response);
  
-     		 
-  } ; 
+				 
+	} ; 
 
--- models/citation.js
+++ models/citation.js
@@ -1,4 +1,26 @@
 var db = require('../configDb');
 
+/*
+* Récupérer l'intégralité des Villes
+* @return Un tableau de Ville avec le N° et le nom
+*/
+module.exports.getListeCitation = function (callback) {	
+   // connection à la base
+	db.getConnection(function(err, connexion){
+		if(!err){
+			var sql = 'SELECT per_nom, cit_libelle, cit_date, AVG(vot_valeur) AS moyenne FROM citation c '+
+				'JOIN personne p ON c.per_num = p.per_num '+
+				'JOIN vote v ON v.cit_num = c.cit_num '+
+				'GROUP BY per_nom, c.cit_num, cit_libelle, cit_date '+
+				'HAVING moyenne IS NOT NULL';
+			// s'il n'y a pas d'erreur de connexion
+			// execution de la requête SQL        	  
+			connexion.query(sql, callback);
+			
+			// la connexion retourne dans le pool
+			connexion.release();
+		 }
+	});   
+};
 
 
