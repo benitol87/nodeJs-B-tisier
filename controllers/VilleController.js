d7aac4373376c16ec980bcade3f844e35bb1048a
guillaume-chs <guillaume.chanson@dbmail.com>
2015-02-11 10:01:38 +0100

    Nouvelle fonctionnalité : ajout d'une ville, et vérif. connection user

--- controllers/VilleController.js
+++ controllers/VilleController.js
@@ -1,57 +1,59 @@
-var model = require('../models/ville.js');
-   
-   // ////////////////////////////////////////////// L I S T E R     V I L L E S 
- 
-/*
-* Ce module permet de récupérer l'intégralité des Villes 
-* en utilisant la méthode getListVille du model ville.js
-* il passe listeVille  et nbVille à la vue listerVille.
-* response.listeVille contient par exemple :
-* [ { vil_num: 5, vil_nom: 'Tulle' },
-* { vil_num: 6, vil_nom: 'Brive' },
-* { vil_num: 17, vil_nom: 'Orléans' } ]
+var model_ville = require('../models/ville.js');
+var home_controller = require('./HomeController.js');
+var view_root = "villes/";
 
-* response.nbVille contient par exemple : 3
-* response.title est passé à main.handlebars via la vue listerVille
-* il sera inclus dans cette balise : <title> {{title}}</title>
-*/ 
-     
+   // ////////////////////////////////////////////// L I S T E R     V I L L E S
+
 module.exports.ListerVille = function(request, response){
-   response.title = 'Liste des villes';
-     
-   model.getListeVille( function (err, result) {
+    response.title = 'Liste des villes';
+
+    model_ville.getListeVille( function (err, result) {
         if (err) {
-            // gestion de l'erreur
             console.log(err);
             return;
         }
-   response.listeVille = result; 
-   response.nbVille = result.length;
-   response.render('listerVille', response);
-        });   
-};   
+        response.listeVille = result;
+        response.nbVille = result.length;
+        response.render(view_root + 'lister', response);
+    });
+};
 
    // ////////////////////////////////////////////// A J O U T E R     V I L L E
-   
-module.exports.AjouterVille = function(request, response){
 
-   response.title = 'Ajouter des villes';
-	
-   response.render('ajoutVille', response);
-};  
- 
-   // ////////////////////////////////////////////// I N S E R E R     V I L L E 
- 
-module.exports.InsertVille = function(request, response){
-    response.title = 'Insertion d\'une ville'; 
- 
- 	response.render('ajoutVille', response);
+module.exports.AjouterVille = function(request, response){
+    if (!request.session.num || !request.session.login) {
+        console.log("vous n'êtes pas connecté");
+        home_controller.Index(request, response);
+        return;
+    }
+
+    if (request.method == "POST") {
+        model_ville.addVille(request.body, function (err, result) {
+            if (err) {
+                console.log(err);
+                return;
+            };
+
+            response.title = "La ville a bien été ajoutée";
+            response.render(view_root + 'ajouter', response);
+        });
+
+    } else {
+       response.title = 'Ajouter des villes';
+       response.render(view_root + 'ajouter', response);
+    }
 };
 
    // ////////////////////////////////////////////// M O D I F I E R     V I L L E
-     
+
 module.exports.ModifierVille = function(request, response){
+    if (!request.session.num || !request.session.login) {
+        console.log("vous n'êtes pas connecté");
+        home_controller.Index(request, response);
+        return;
+    }
+
    response.title = 'Modifier une ville';
-   response.render('modifierVille', response);
-}; 
+   response.render(view_root + 'modifier', response);
+};
 
