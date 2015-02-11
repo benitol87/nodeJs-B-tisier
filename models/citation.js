8baff0dd00f6697d18c7b6f93bf7d996a13638d7
guillaume-chs <guillaume.chanson@dbmail.com>
2015-02-11 09:24:38 +0100

    Nouvelle fonctionnalité : ajout d'une citation

--- models/citation.js
+++ models/citation.js
@@ -1,13 +1,13 @@
 var db = require('../configDb');
 
 /*
-* Récupérer une partie des citations (celles notées, dont la date ne vaut pas NULL, 
+* Récupérer une partie des citations (celles notées, dont la date ne vaut pas NULL,
 * et qui sont valides)
 * @return Un tableau de citations avec le nom de la personne,
 * la citation, la date et la moyenne des notes
 */
-module.exports.getListeCitation = function (callback) {	
-   // connection à la base
+module.exports.getListeCitation = function (callback) {
+    // connection à la base
 	db.getConnection(function(err, connexion){
 		if(!err){
 			var sql = 'SELECT per_nom, cit_libelle, DATE_FORMAT(cit_date, "%d/%m/%Y") AS cit_date, AVG(vot_valeur) AS moyenne FROM citation c '+
@@ -17,13 +17,21 @@ module.exports.getListeCitation = function (callback) {
 				'GROUP BY per_nom, c.cit_num, cit_libelle, cit_date '+
 				'HAVING moyenne IS NOT NULL';
 			// s'il n'y a pas d'erreur de connexion
-			// execution de la requête SQL        	  
+			// execution de la requête SQL
 			connexion.query(sql, callback);
-			
+
 			// la connexion retourne dans le pool
 			connexion.release();
 		 }
-	});   
+	});
 };
 
-
+module.exports.addCitation = function (data, callback) {
+    // connection à la base
+	db.getConnection(function(err, connexion){
+		if(!err){
+			connexion.query('INSERT INTO citation SET ?', data, callback);
+			connexion.release();
+		 }
+	});
+}
